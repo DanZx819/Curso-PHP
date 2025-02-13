934637f11ba9849a9bbf4739d68825ce072eefe5
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-02-13 19:13:39 -0300

    Desafio 2

---
+++ Desafios/Desafio 2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Randomizador de números</title>
+</head>
+<body>
+    <section class="container1">
+        <div class="container-box">
+            <h1>Randomizador de números</h1>
+            <p>Ao clicar no botão abaixo será escolhido um número de 0 a 100.</p>
+            <form method="get" class="form-box">
+                <button type="submit" name="btn">Gerar</button>
+            </form>
+
+            <?php 
+                if (isset($_GET["btn"])) {
+                    $rand = rand(0, 100);
+                    print "<p>Seu número foi $rand</p>";
+                }
+            ?>
+
+        </div>
+    </section>
+</body>
+</html>
