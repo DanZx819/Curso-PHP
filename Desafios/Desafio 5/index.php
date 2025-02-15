7b06fd7157b792f8d6cf6486a2a6c75cd3a1bad9
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-02-15 00:15:53 -0300

    commit

---
+++ Desafios/Desafio 5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Analisador de números.</title>
+</head>
+<body>
+    <section class="container1">
+        
+            
+            <form action="res.php" method="get" class="form-box">
+                <h1>Analisador de numeros</h1>
+                <input type="number" name="num" placeholder="Digie o seu número:" step="any" require></input>
+                <button type="submit" method="res.php">Analisar</button>
+            </form>
+        
+    </section>
+</body>
+</html>
---
+++ Desafios/Desafio 5/res.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section class="container1">
+        
+            
+        <form method="get" class="form-box">
+            <h1>Analisador de numeros</h1>
+            <?php
+                if(isset($_GET["num"])){
+                    $num = $_GET["num"];
+                    $x = explode('.', (string)$num);
+                    $int = $x[0];
+                    $frac = $x[1];
+                    echo "<p class ='php-p'>Seu número: $num <br> Parte inteira: $int <br> Parte decimal: $frac </p>";
+                }else{
+                    print"<p class = 'php-p'>Digite um valor válido</p>";
+                }
+                ?>
+            <button type="button" onclick="window.history.back()">Voltar</button>
+           
+        </form>
+    
+</section>
+</body>
+</html>
